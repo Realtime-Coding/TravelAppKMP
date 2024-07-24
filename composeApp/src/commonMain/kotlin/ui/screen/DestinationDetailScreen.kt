4c689e9f2aed7fea069fa2af0abd1bb187035cf3
Shahzad Afridi <shahzadahmadafridi@gmail.com>
2024-07-24 15:09:36 +0500

    Card Item added to cart screen

--- composeApp/src/commonMain/kotlin/ui/screen/DestinationDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/DestinationDetailScreen.kt
@@ -94,7 +94,7 @@ fun DestinationDetailScreenView(
             rememberThumbnail.value = it
         }
         PrimaryButton(
-            title = "Book Now",
+            title = "Add to Cart",
             paddingValues = PaddingValues(start = 25.dp, top = 36.dp, end = 25.dp, bottom = 36.dp),
             onClick = { viewModel.addToCart(destination) }
         )
